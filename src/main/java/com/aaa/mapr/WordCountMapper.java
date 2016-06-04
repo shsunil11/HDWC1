22174481644065227da5dbfd98e0245dddcdf3b3
shsunil11 <sunil.shevale@gmail.com>
2016-06-04 14:29:09 -0700

    Initial Commit

---
+++ src/main/java/com/aaa/mapr/WordCountMapper.java
@@ -0,0 +1,21 @@
+package com.aaa.mapr;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+   public class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+      final private static LongWritable ONE = new LongWritable(1);
+      private Text tokenValue = new Text();
+
+      @Override
+      protected void map(LongWritable offset, Text text, Context context) throws IOException, InterruptedException {
+         for (String token : text.toString().split("\\s+")) {
+            tokenValue.set(token);
+            context.write(tokenValue, ONE);
+         }
+      }
+   }
